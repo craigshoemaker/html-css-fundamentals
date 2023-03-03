ae4ea700f4b955a38b8f5bc4d58b150cd87a2800
craigshoemaker <craig@craigshoemaker.net>
2023-03-02 22:25:36 -0700

    Update layout & reorganize JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
-    const inputBoxes = document.querySelectorAll('#calculator input');
-    const totalContainer = document.getElementById('total-container');
-
     const inventory = {
         apple: { price: 12, qty: 0 },
         cherry: { price: 15, qty: 0 },
         strawberry: { price: 18, qty: 0 }
     };
-
-    function calculate(box) {
-        let qty = 0;
-        if(box.value.length > 0) {
-            qty = parseInt(box.value);
-        }
-        inventory[box.id].qty = qty;
-        sum();
-    }
 
     function sum() {
         let total = 0;
@@ -24,10 +12,27 @@ window.addEventListener('DOMContentLoaded', () => {
         keys.forEach(key => {
             total += inventory[key].price * inventory[key].qty;
         });
+        return total;
+    }
+
+    const totalContainer = document.getElementById('total-container');
+
+    function calculate(box) {
+        let qty = 0;
+
+        if(box.value.length > 0) {
+            qty = parseInt(box.value);
+        }
+
+        inventory[box.id].qty = qty;
+
+        let total = sum();
         totalContainer.innerHTML = `$${total}.00`;
     }
     
+    const inputBoxes = document.querySelectorAll('#calculator input');
     const quantityBoxes = Array.from(inputBoxes);
+    
     quantityBoxes.forEach(box => {
         box.addEventListener('change', () => { calculate(box) });
         box.addEventListener('keyup', () => { calculate(box) });
