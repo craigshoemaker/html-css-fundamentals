02e5eac8b75b2b973998729dd48dd0a88c146f38
Craig Shoemaker <craig@craigshoemaker.net>
2023-03-12 07:25:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
+const inventory = {
+    apple: { price: 12, qty: 0 },
+    cherry: { price: 15, qty: 0 },
+    strawberry: { price: 18, qty: 0 }
+};
+
+function sum() {
+    let total = 0;
+    const keys = Object.keys(inventory);
+    keys.forEach(key => {
+        total += inventory[key].price * inventory[key].qty;
+    });
+    return total;
+}
+
+function calculate(box) {
+    let qty = 0;
+
+    if(box.value.length > 0) {
+        qty = parseInt(box.value);
+    }
+
+    inventory[box.id].qty = qty;
+
+    const total = sum();
+    return `$${total}.00`;
+}
+
 window.addEventListener('DOMContentLoaded', () => { 
 
-    const inventory = {
-        apple: { price: 12, qty: 0 },
-        cherry: { price: 15, qty: 0 },
-        strawberry: { price: 18, qty: 0 }
-    };
-
-    function sum() {
-        let total = 0;
-        const keys = Object.keys(inventory);
-        keys.forEach(key => {
-            total += inventory[key].price * inventory[key].qty;
-        });
-        return total;
-    }
-
     const totalContainer = document.getElementById('total-container');
-
-    function calculate(box) {
-        let qty = 0;
-
-        if(box.value.length > 0) {
-            qty = parseInt(box.value);
-        }
-
-        inventory[box.id].qty = qty;
-
-        let total = sum();
-        totalContainer.innerHTML = `$${total}.00`;
-    }
-    
     const inputBoxes = document.querySelectorAll('#calculator input');
+
     const quantityBoxes = Array.from(inputBoxes);
-    
     quantityBoxes.forEach(box => {
-        box.addEventListener('change', () => { calculate(box) });
-        box.addEventListener('keyup', () => { calculate(box) });
+        
+        box.addEventListener('change', () => { 
+            totalContainer.innerHTML = calculate(box);
+        });
+        
+        box.addEventListener('keyup', () => { 
+            totalContainer.innerHTML = calculate(box);
+        });
+        
     });
+    
 });
